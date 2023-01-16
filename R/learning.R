57a73b00680e696e88f398a2043b2a07c0a4ac32
Evgeniya Pavlova <evepavlova98@gmail.com>
2023-01-16 16:02:43 +0100

    Add learning.R to git

---
+++ R/learning.R
@@ -0,0 +1,18 @@
+10
+
+# R_basics ----------------------------------------------------------------
+
+weight_kilos <- 100
+weight_kilos <- 10
+colnames(airquality)
+str(airquality)
+summary(airquality)
+
+
+2 + 2
+2 + 2
+
+# Packages ----------------------------------------------------------------
+
+library(tidyverse)
+
