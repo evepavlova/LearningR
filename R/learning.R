478d0419684a0fea2fbdc630717db21db8f806a5
Evgeniya Pavlova <evepavlova98@gmail.com>
2023-01-17 11:15:40 +0100

    Made smaller nhanes dataset

--- R/learning.R
+++ R/learning.R
@@ -1,20 +1,53 @@
-#Merge conflict example
-10
+# Merge conflict example
+# 10
 
 # R_basics ----------------------------------------------------------------
 
-weight_kilos <- 100
-weight_kilos <- 10
-colnames(airquality)
-str(airquality)
-summary(airquality)
+# weight_kilos <- 100
+# weight_kilos <- 10
+# colnames(airquality)
+# str(airquality)
+# summary(airquality)
 
 
-2 + 2
-2 + 2
+# 2 + 2
+# 2 + 2
 
 # Packages ----------------------------------------------------------------
 
-library(tidyverse)
+# library(tidyverse)
 
-#this will be used for testing out Git
+# this will be used for testing out Git
+
+
+# Learning data wrangling -------------------------------------------------
+
+library(tidyverse)
+library(NHANES)
+
+# Looking at data ---------------------------------------------------------
+
+glimpse(NHANES)
+colnames(NHANES)
+
+select(NHANES, Age, Weight, BMI)
+
+select(NHANES, -HeadCirc)
+
+select(NHANES, starts_with("BP"), Age)
+select(NHANES, ends_with("Day"))
+select(NHANES, contains("Age"))
+
+nhanes_small <- select(
+  NHANES,
+  ID,
+  Age,
+  Gender,
+  BMI,
+  Diabetes,
+  PhysActive,
+  BPSysAve,
+  BPDiaAve,
+  Education
+)
+nhanes_small
